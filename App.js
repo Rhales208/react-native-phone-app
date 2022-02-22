edf964640b10fd00479fb0ffa97b9b17a7435278
RuebenHales <halesrueben@gmail.com>
2022-02-22 16:23:56 -0500

    first native app

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>heyyyy</Text>
+      <Button 
+      title="Press Me!"
+       onPress={() => Alert.alert("You pressed the button!")}
+      color="white"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +21,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  
 });
